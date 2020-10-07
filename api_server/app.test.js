10c428912eebc953b970c2667c15051d2f5856dc
Ming Xiao <mxiao@pivotal.io>
2020-10-06 19:39:40 -0700

    Read port from env
    [#175110237]

--- api_server/app.test.js
+++ api_server/app.test.js
@@ -1,7 +1,9 @@
 const {app} = require ('./app');
 const request = require('supertest');
 
-test('get index', async () => {
-  const res = await request(app).get('/');
-  expect(res.status).toBe(200)
+describe('/', () => {
+  test('GET', async () => {
+    const res = await request(app).get('/');
+    expect(res.status).toBe(200)
+  });
 })
