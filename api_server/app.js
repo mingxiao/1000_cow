5e9ec89d9d49c7608e68379084207c2e2d870a7d
Ming Xiao <mxiao@pivotal.io>
2020-10-10 20:43:34 -0700

    Handle invalid table name on /data endpoint
    [#175158350]

--- api_server/app.js
+++ api_server/app.js
@@ -1,9 +1,31 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 5000
+const { Sequelize } = require('sequelize');
+
+let db_str = '';
+if (process.env.ENV == 'test') {
+  db_str = 'sqlite::memory:'
+}
+const sequelize = new Sequelize(db_str)
 
 app.get('/', (req, res) => {
   res.send('Hello World')
+})
+
+const VALID_TABLES = ['taxonomic_groups']
+
+app.get('/data', (req, res) => {
+  const table_name = req.query.table_name
+  if (!VALID_TABLES.includes(table_name)){
+    const body = {
+      "errors": [{
+        "title": "Unknown Table",
+        "detail": `Unknown Table: ${table_name}`
+      }]
+    }
+    res.status(400).send(body)
+  }
 })
 
 const server = app.listen(port, () => {
--- api_server/app.test.js
+++ api_server/app.test.js
@@ -8,6 +8,39 @@ describe('/', () => {
   });
 })
 
+describe('/data', () => {
+  describe('when the table does not exist', () => {
+    test('it returns 400 with error message', async () => {
+      const res = await request(app).get('/data?table_name=non_exist');
+      expect(res.status).toBe(400)
+      const expectedBody = {
+        "errors": [{
+          "title": "Unknown Table",
+          "detail": `Unknown Table: non_exist`
+        }]
+      };
+      expect(res.body).toEqual(expectedBody);
+    })
+  })
+
+  describe('when db connection fails', () => {
+    // TODO simulate database error
+    xtest('it returns 403 with error body', async () => {
+      const res = await request(app).get('/data');
+
+      expect(res.status).toBe(403)
+      const expectedBody = {
+        "errors": [{
+          "title": "Database Unreachable",
+          "detail": "Could not establish connection to the database"
+        }]
+      };
+      expect(res.body).toEqual(expectedBody);
+    });
+  });
+
+});
+
 afterAll(() => {
   server.close()
 });
