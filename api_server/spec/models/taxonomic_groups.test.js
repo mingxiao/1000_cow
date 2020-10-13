f9aa984934a173419651d030abfb5a83c31ef5de
Ming Xiao <mxiao@pivotal.io>
2020-10-12 21:19:30 -0700

    We cannot de-structure from commonjs modules
    [#175135968]

--- api_server/spec/models/taxonomic_groups.test.js
+++ api_server/spec/models/taxonomic_groups.test.js
@@ -1,10 +1,9 @@
-const {TaxononmicGroups} = require('../../built/models/taxonomic_groups')
-// import {TaxonomicGroups} from '../../built/models/taxonomic_groups'
+const TaxononmicGroups = require('../../built/models/taxonomic_groups')
 
 fdescribe('TaxonomicGroups', () => {
   describe('create', () => {
     test('it has the correct arguments', async () => {
-      const group = await TaxononmicGroups.create({})
+      const group = await TaxononmicGroups.TaxonomicGroups.create({})
     })
   })
 })
